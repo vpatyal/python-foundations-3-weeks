2ec0639f9e30ae8bab0ba711c6ca3754c3bcedaa
vpatyal <vinod.patyal@rhsmith.umd.edu>
2022-12-04 10:50:09 -0500

    add logic to show the largest collatz list and also the maximum number

--- problems/problem_3_collatz.py
+++ problems/problem_3_collatz.py
@@ -33,12 +33,25 @@ def collatz(num):
     while num >= 1:
         list_of_nums.append(num)
         if num == 1:            
-            print(f"collatz list for {x} is {list_of_nums}")
+            print(f"\ncollatz list for {x} is {list_of_nums}")
+            print(f"length of list: {len(list_of_nums)}, maximum number in list: {max(list_of_nums)}")
+            return [len(list_of_nums), max(list_of_nums)]
             break
         else:
             num = next_num(num)
 
 # Todo: print the Collatz sequence for each number from 1 - 100
+a = [0, 0]  # list to hold the maximum number of elements from collatz list of 1 -100
+b = [0, 0]  # list to hold the largest number from collatz list of 1 - 100
 for n in range(100, 1, -1):
     list_of_nums = []
-    collatz(n)
+    ret_list_next_num = collatz(n)
+    if ret_list_next_num[0] > a[1]:
+        a[0] = n
+        a[1] = ret_list_next_num[0]
+    if ret_list_next_num[1] > b[1]:
+        b[0] = n
+        b[1] = ret_list_next_num[1]
+
+print(f"\n\n\nNumber {a[0]} has the maximum number of elements ({a[1]}) in collatz list")
+print(f"Number {b[0]} has the largest element ({b[1]}) in collatz lists for numbers 1-100")
